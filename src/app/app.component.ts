0e12507f233c4842adaf0f66601acee6a8ecba36
Jaime <jaime99dr@gmail.com>
2022-09-14 18:28:43 +0100

    geoApi request using client's public IP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import * as L from 'leaflet';
+import { GeoApiService } from './services/geo-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,AfterViewInit {
   map: any;
+
+  constructor(private geoApiService: GeoApiService) { }
   
   ngOnInit(): void {
+    this.geoApiService.prueba();
+  }
+
+  ngAfterViewInit(): void {
+    console.log("2");
     this.map = new L.Map('map', {
       center: [ 39.8282, -98.5795 ],
       zoom: 3
@@ -20,7 +28,8 @@ export class AppComponent implements OnInit {
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
-
+    
     tiles.addTo(this.map);
+    console.log("3");
   }
 }
--- src/app/services/geo-api.service.ts
+++ src/app/services/geo-api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeoApiService {
 
-  constructor() { }
+  prueba() {
+    console.log("prueba");
+/*     axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_84JL1NuHF9CpQSxcG34d2FWlRgyGr').then(res => {
+      console.log("respuesta con IP pública del usuario: ", res);
+    }); */
+  }
 }
